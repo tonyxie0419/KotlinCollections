6d8c536de050f11020e3393d960b66984da794f4
tonyxie <tonyxie_0419@qq.com>
2017-08-18 15:30:13 +0800

    集合中max,min的使用方法，注意：可以和以前的方法组合使用

---
+++ src/MaxMin.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println(shop.getCustomerWithMaximumNumberOfOrders())
+
+    println(customerAfter[nathan]!!.getMostExpensiveOrderedProduct())
+}
+
+// Return a customer whose order count is the highest among all customers
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size }
+
+// Return the most expensive product which has been ordered
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders.flatMap { it.products }.maxBy { it.price }
