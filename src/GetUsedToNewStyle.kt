8f89c1846ee14d0ba47acc05a0ff61778aed18aa
tonyxie <tonyxie_0419@qq.com>
2017-08-22 15:09:02 +0800

    集合的多种用法组合

---
+++ src/GetUsedToNewStyle.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+}
+
+fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+
+    val groupsByLength = collection. groupBy { s -> s.length }
+
+    val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.max()
+
+    return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
+}
